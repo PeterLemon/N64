54f443089e0fb227e4babb71f1bb9328402aa895
PeterLemon <krom@gbadev.org>
2014-10-05 07:05:58 +0100

    Code Cleanup

--- RDP/16BPP/Triangle/Cube/FillTriangle320x240/CubeFillTriangle16BPP320X240.asm
+++ RDP/16BPP/Triangle/Cube/FillTriangle320x240/CubeFillTriangle16BPP320X240.asm
@@ -1,57 +1,56 @@
 ; N64 'Bare Metal' 16BPP 320x240 Cube Fill Triangle RDP Demo by krom (Peter Lemon):
-
   include LIB\N64.INC ; Include N64 Definitions
-  dcb 2097152,$00 ; Set ROM Size
+  dcb 1048576,$00 ; Set ROM Size
   org $80000000 ; Entry Point Of Code
   include LIB\N64_HEADER.ASM  ; Include 64 Byte Header & Vector Table
   incbin LIB\N64_BOOTCODE.BIN ; Include 4032 Byte Boot Code
 
 Start:
-  include LIB\N64_INIT.ASM ; Include Initialisation Routine
-  include LIB\N64_GFX.INC  ; Include Graphics Macros
+  include LIB\N64_GFX.INC ; Include Graphics Macros
   include LIB\N64_3DCP1.INC ; Include 3D CP1 Macros
+  N64_INIT ; Run N64 Initialisation Routine
 
-  ScreenNTSC 320,240, BPP16, $A0100000 ; Screen NTSC: 320x240, 16BPP, DRAM Origin $A0100000
+  ScreenNTSC 320, 240, BPP16, $A0100000 ; Screen NTSC: 320x240, 16BPP, DRAM Origin $A0100000
 
-  la t0,MULT ; T0 = Float Multipy Data Offset
-  lwc1 f0,0(t0) ; F0 = 0.0 (Divide By Zero Check)
-  lwc1 f1,4(t0) ; F1 = 4.0 (Fixed Point S.11.2)
-  lwc1 f2,8(t0) ; F2 = 65536.0 (Fixed Point S.15.16)
+  la a0,MULT ; A0 = Float Multipy Data Offset
+  lwc1 f0,0(a0) ; F0 = 0.0 (Divide By Zero Check)
+  lwc1 f1,4(a0) ; F1 = 4.0 (Fixed Point S.11.2)
+  lwc1 f2,8(a0) ; F2 = 65536.0 (Fixed Point S.15.16)
 
-  la t0,Matrix3D ; T0 = Float Matrix 3D Data Offset
-  lwc1 f3,0(t0)   ; F3  = Matrix3D[0]
-  lwc1 f4,4(t0)   ; F4  = Matrix3D[1]
-  lwc1 f5,8(t0)   ; F5  = Matrix3D[2]
-  lwc1 f6,12(t0)  ; F6  = Matrix3D[3]
-  lwc1 f7,16(t0)  ; F7  = Matrix3D[4]
-  lwc1 f8,20(t0)  ; F8  = Matrix3D[5]
-  lwc1 f9,24(t0)  ; F9  = Matrix3D[6]
-  lwc1 f10,28(t0) ; F10 = Matrix3D[7]
-  lwc1 f11,32(t0) ; F11 = Matrix3D[8]
-  lwc1 f12,36(t0) ; F12 = Matrix3D[9]
-  lwc1 f13,40(t0) ; F13 = Matrix3D[10]
-  lwc1 f14,44(t0) ; F14 = Matrix3D[11]
+  la a0,Matrix3D ; A0 = Float Matrix 3D Data Offset
+  lwc1 f3,0(a0)   ; F3  = Matrix3D[0]
+  lwc1 f4,4(a0)   ; F4  = Matrix3D[1]
+  lwc1 f5,8(a0)   ; F5  = Matrix3D[2]
+  lwc1 f6,12(a0)  ; F6  = Matrix3D[3]
+  lwc1 f7,16(a0)  ; F7  = Matrix3D[4]
+  lwc1 f8,20(a0)  ; F8  = Matrix3D[5]
+  lwc1 f9,24(a0)  ; F9  = Matrix3D[6]
+  lwc1 f10,28(a0) ; F10 = Matrix3D[7]
+  lwc1 f11,32(a0) ; F11 = Matrix3D[8]
+  lwc1 f12,36(a0) ; F12 = Matrix3D[9]
+  lwc1 f13,40(a0) ; F13 = Matrix3D[10]
+  lwc1 f14,44(a0) ; F14 = Matrix3D[11]
 
 Loop:
   WaitScanline $1E0 ; Wait For Scanline To Reach Vertical Blank
-  la t2,$A0000000|(RDPTri&$3FFFFF) ; T2 = RDP Triangle Buffer Offset
+  la a2,$A0000000|(RDPTri&$3FFFFF) ; A2 = RDP Triangle Buffer Offset
 
-  la t0,XRot ; T0 = X Rotation Data Offset
+  la a0,XRot ; A0 = X Rotation Data Offset
 
-  lw t1,0(t0)  ; Load X Rotation Value
-  addi t1,1    ; X Rotation += 1
-  andi t1,1023 ; X Rotation &= 1023
-  sw t1,0(t0)  ; Store X Rotation Value
+  lw t0,0(a0)  ; Load X Rotation Value
+  addi t0,1    ; X Rotation += 1
+  andi t0,1023 ; X Rotation &= 1023
+  sw t0,0(a0)  ; Store X Rotation Value
 
-  lw t1,4(t0)  ; Load Y Rotation Value
-  addi t1,1    ; Y Rotation += 1
-  andi t1,1023 ; Y Rotation &= 1023
-  sw t1,4(t0)  ; Store Y Rotation Value
+  lw t0,4(a0)  ; Load Y Rotation Value
+  addi t0,1    ; Y Rotation += 1
+  andi t0,1023 ; Y Rotation &= 1023
+  sw t0,4(a0)  ; Store Y Rotation Value
 
-  lw t1,8(t0)  ; Load Z Rotation Value
-  addi t1,1    ; Z Rotation += 1
-  andi t1,1023 ; Z Rotation &= 1023
-  sw t1,8(t0)  ; Store Z Rotation Value
+  lw t0,8(a0)  ; Load Z Rotation Value
+  addi t0,1    ; Z Rotation += 1
+  andi t0,1023 ; Z Rotation &= 1023
+  sw t0,8(a0)  ; Store Z Rotation Value
 
   ;XRotCalc XRot, SinCos1024 ; X Rotate
   ;YRotCalc YRot, SinCos1024 ; Y Rotate
@@ -79,16 +78,16 @@ Loop:
   XYZPos CubeFPos ; Translate Cube F
   FillTriRightCullFront CubeTri, CubeTriEnd ; Load Object, Fill Triangle, Right Major, Front Face Culling: Start Address, End Address
 
-  ; Run Generated RDP Command List
-  lui t0,DPC_BASE ; Reality Display Processer Control Interface
-  la t1,$A0000000|(RDPBuffer&$3FFFFF) ; Set DPC Command Start Address
-  sw t1,DPC_START(t0) ; Store DPC Command Start Address To DP Start Reg
-  sw t2,DPC_END(t0) ; Store DPC Command End Address To DP End Reg
+  ; Run Generated RDP Command List (A2 = DPC Command End Address)
+  lui a0,DPC_BASE ; A0 = Reality Display Processer Control Interface Base Register ($A4100000)
+  la a1,$A0000000|(RDPBuffer&$3FFFFF) ; A1 = DPC Command Start Address
+  sw a1,DPC_START(a0) ; Store DPC Command Start Address To DP Start Register ($A4100000)
+  sw a2,DPC_END(a0) ; Store DPC Command End Address To DP End Register ($A4100004)
 
   j Loop
   nop ; Delay Slot
 
-  align 4 ; Align 32-bit
+  align 4 ; Align 32-Bit
 MULT: ; Float Multipy Data
   IEEE32     0.0 ; (Divide By Zero Check)
   IEEE32     4.0 ; Multiply (Fixed Point S.11.2)
@@ -118,7 +117,7 @@ FOV:           IEEE32 160.0
   include scene.asm ; Scene Data
   include sincos1024.asm ; Pre Calculated Matrix Sin Cos Rotation Values
 
-  align 8 ; Align 64-bit
+  align 8 ; Align 64-Bit
 RDPBuffer:
   Set_Scissor 0<<2,0<<2, 320<<2,240<<2, 0 ; Set Scissor: XH 0.0, YH 0.0, XL 320.0, YL 240.0, Scissor Field Enable Off
   Set_Other_Modes CYCLE_TYPE_FILL, 0 ; Set Other Modes
