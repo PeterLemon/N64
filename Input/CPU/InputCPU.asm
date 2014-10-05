36a4c0d2c5251fa210f7db346477f746f5604dea
PeterLemon <krom@gbadev.org>
2014-10-05 05:03:43 +0100

    Code Cleanup

--- Input/CPU/InputCPU.asm
+++ Input/CPU/InputCPU.asm
@@ -1,65 +1,65 @@
 ; N64 'Bare Metal' Input CPU Demo by krom (Peter Lemon):
-
   include LIB\N64.INC ; Include N64 Definitions
-  include LIB\N64_INPUT.INC ; Include Input Macros
-  include LIB\N64_GFX.INC ; Include Graphics Macros
-  dcb 2097152,$00 ; Set ROM Size
+  dcb 1048576,$00 ; Set ROM Size
   org $80000000 ; Entry Point Of Code
   include LIB\N64_HEADER.ASM  ; Include 64 Byte Header & Vector Table
   incbin LIB\N64_BOOTCODE.BIN ; Include 4032 Byte Boot Code
 
 Start:
-  include LIB\N64_INIT.ASM ; Include Initialisation Routine
+  include LIB\N64_GFX.INC ; Include Graphics Macros
+  include LIB\N64_INPUT.INC ; Include Input Macros
+  N64_INIT ; Run N64 Initialisation Routine
 
-  ScreenNTSC 320,240, BPP32, $A0100000 ; Screen NTSC: 320x240, 32BPP, DRAM Origin $A0100000
+  ScreenNTSC 320, 240, BPP32, $A0100000 ; Screen NTSC: 320x240, 32BPP, DRAM Origin $A0100000
 
-  lui t0,$A010 ; T0 = VRAM Start Offset
-  addi t1,t0,((320*240*4)-4) ; T1 = VRAM End Offset
-  li t2,$000000FF ; T2 = Black
+  lui a0,$A010 ; A0 = VRAM Start Offset
+  addi a1,a0,((320*240*4)-4) ; A1 = VRAM End Offset
+  li t0,$000000FF ; T0 = Black
 ClearScreen:
-  sw t2,0(t0)
-  bne t0,t1,ClearScreen
-  addi t0,4 ; Delay Slot
+  sw t0,0(a0)
+  bne a0,a1,ClearScreen
+  addi a0,4 ; VRAM += 4
 
-  li t3,($A0100000+(120*320*4)+(160*4))
+  la a3,($A0100000+(120*320*4)+(160*4)) ; A3 = Pixel Position
   
   InitController PIF1 ; Initialize Controller
 
 Loop:
-  WaitScanline $200 ; Wait For Scanline To Reach Vertical Blank
+  WaitScanline $1E0 ; Wait For Scanline To Reach Vertical Blank
+
   ReadController PIF2 ; T0 = Controller Buttons, T1 = Analog X, T2 = Analog Y
 
-  andi t1,t0,JOY_UP ; Test JOY UP
-  beqz t1,Down
+  andi t3,t0,JOY_UP ; Test JOY UP
+  beqz t3,Down
   nop ; Delay Slot
-  subi t3,(320*4)
+  subi a3,(320*4)
 
 Down:
-  andi t1,t0,JOY_DOWN ; Test JOY DOWN
-  beqz t1,Left
+  andi t3,t0,JOY_DOWN ; Test JOY DOWN
+  beqz t3,Left
   nop ; Delay Slot
-  addi t3,(320*4)
+  addi a3,(320*4)
 
 Left:
-  andi t1,t0,JOY_LEFT ; Test JOY LEFT
-  beqz t1,Right
+  andi t3,t0,JOY_LEFT ; Test JOY LEFT
+  beqz t3,Right
   nop ; Delay Slot
-  subi t3,4
+  subi a3,4
 
 Right:
-  andi t1,t0,JOY_RIGHT ; Test JOY RIGHT
-  beqz t1,Render
+  andi t3,t0,JOY_RIGHT ; Test JOY RIGHT
+  beqz t3,Render
   nop ; Delay Slot
-  addi t3,4
+  addi a3,4
 
 Render:
-  li t1,$FFFFFFFF
-  sw t1,0(t3)
+  li t0,$FFFFFFFF
+  sw t0,0(a3)
 
   j Loop
   nop ; Delay Slot
 
-  align 8 ; Align 64-bit
+  align 8 ; Align 64-Bit
 PIF1:
   dw $FF010401,0
   dw 0,0
--- Input/RDP/InputRDP.asm
+++ Input/RDP/InputRDP.asm
@@ -1,88 +1,84 @@
 ; N64 'Bare Metal' Input RDP Demo by krom (Peter Lemon):
-
   include LIB\N64.INC ; Include N64 Definitions
-  include LIB\N64_INPUT.INC ; Include Input Macros
-  include LIB\N64_GFX.INC ; Include Graphics Macros
-  dcb 2097152,$00 ; Set ROM Size
+  dcb 1048576,$00 ; Set ROM Size
   org $80000000 ; Entry Point Of Code
   include LIB\N64_HEADER.ASM  ; Include 64 Byte Header & Vector Table
   incbin LIB\N64_BOOTCODE.BIN ; Include 4032 Byte Boot Code
 
 Start:
-  include LIB\N64_INIT.ASM ; Include Initialisation Routine
+  include LIB\N64_GFX.INC ; Include Graphics Macros
+  include LIB\N64_INPUT.INC ; Include Input Macros
+  N64_INIT ; Run N64 Initialisation Routine
 
-  ScreenNTSC 320,240, BPP32, $A0100000 ; Screen NTSC: 320x240, 32BPP, DRAM Origin $A0100000
+  ScreenNTSC 320, 240, BPP32, $A0100000 ; Screen NTSC: 320x240, 32BPP, DRAM Origin $A0100000
 
-  li t6,160 ; T6 = X Position (1/2 Screen X)
-  li t7,120 ; T7 = Y Position (1/2 Screen Y)
+  li t5,160 ; T5 = X Position (1/2 Screen X)
+  li t6,120 ; T6 = Y Position (1/2 Screen Y)
+
+  la a3,$A0000000|(FillRect&$3FFFFF) ; A0 = Fill Rect RAM Offset
 
   InitController PIF1 ; Initialize Controller
 
 Loop:
   WaitScanline $200 ; Wait For Scanline To Reach Vertical Blank
 
-  DPC RDPBuffer,RDPBufferEnd ; Run DPC Command Buffer: Start, End
+  DPC RDPBuffer, RDPBufferEnd ; Run DPC Command Buffer: Start, End
 
   ReadController PIF2 ; T0 = Controller Buttons, T1 = Analog X, T2 = Analog Y
 
-  li t1,8
-  beq t7,t1,Down ; IF(Y = 0) GOTO Down
-  
-  andi t1,t0,JOY_UP ; Test JOY UP
-  beq t1,r0,Down
-  nop ; Delay Slot
-  subi t7,1
-
-Down:
-  li t1,232
-  beq t7,t1,Left ; IF(Y = 232) GOTO Left
-
-  andi t1,t0,JOY_DOWN ; Test JOY DOWN
-  beq t1,r0,Left
-  nop ; Delay Slot
-  addi t7,1
-
-Left:
-  li t1,8
-  beq t6,t1,Right ; IF(X = 8) GOTO Right
-
-  andi t1,t0,JOY_LEFT ; Test JOY LEFT
-  beq t1,r0,Right
+  li t3,8
+  beq t6,t3,Down ; IF (Y = 0) Down (Screen Edge Collision)
+  andi t3,t0,JOY_UP ; Test JOY UP
+  beqz t3,Down
   nop ; Delay Slot
   subi t6,1
 
-Right:
-  li t1,312
-  beq t6,t1,Render ; IF(X = 312) GOTO Render
-
-  andi t1,t0,JOY_RIGHT ; Test JOY RIGHT
-  beq t1,r0,Render
+Down:
+  li t3,232
+  beq t6,t3,Left ; IF (Y = 232) Left (Screen Edge Collision)
+  andi t3,t0,JOY_DOWN ; Test JOY DOWN
+  beqz t3,Left
   nop ; Delay Slot
   addi t6,1
 
-Render:
-  la t0,$A0000000|(FillRect&$3FFFFF) ; T0 = Fill Rect RAM Offset
-  addi t1,t6,7 ; T1 = XL
-  addi t2,t7,7 ; T2 = YL
-  subi t3,t6,8 ; T3 = XH
-  subi t4,t7,8 ; T4 = YH
-  dsll32 t1,14
-  dsll32 t2,2
-  dsll t3,14
-  dsll t4,2
+Left:
+  li t3,8
+  beq t5,t3,Right ; IF (X = 8) Right (Screen Edge Collision)
+  andi t3,t0,JOY_LEFT ; Test JOY LEFT
+  beqz t3,Right
+  nop ; Delay Slot
+  subi t5,1
 
-  lui t5,$3600 ; T5 = Fill Rect RDP Command
-  dsll32 t5,0
-  or t5,t1
-  or t5,t2
-  or t5,t3
-  or t5,t4
-  sd t5,0(t0) ; Store RDP Command
+Right:
+  li t3,312
+  beq t5,t3,Render ; IF (X = 312) Render (Screen Edge Collision)
+  andi t3,t0,JOY_RIGHT ; Test JOY RIGHT
+  beqz t3,Render
+  nop ; Delay Slot
+  addi t5,1
+
+Render:
+  addi t0,t5,7 ; T0 = XL
+  addi t1,t6,7 ; T1 = YL
+  subi t2,t5,8 ; T2 = XH
+  subi t3,t6,8 ; T3 = YH
+  dsll32 t0,14
+  dsll32 t1,2
+  dsll t2,14
+  dsll t3,2
+
+  lui t4,$3600 ; T4 = Fill Rect RDP Command
+  dsll32 t4,0
+  or t4,t0
+  or t4,t1
+  or t4,t2
+  or t4,t3
+  sd t4,0(a3) ; Store RDP Command
 
   j Loop
   nop ; Delay Slot
 
-  align 8 ; Align 64-bit
+  align 8 ; Align 64-Bit
 RDPBuffer:
   Set_Scissor 0<<2,0<<2, 320<<2,240<<2, 0 ; Set Scissor: XH 0.0, YH 0.0, XL 320.0, YL 240.0, Scissor Field Enable Off
   Set_Other_Modes CYCLE_TYPE_FILL, 0 ; Set Other Modes
