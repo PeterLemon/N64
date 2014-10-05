3caa2f2b14d253923e13ab570805c261875b32d3
PeterLemon <krom@gbadev.org>
2014-10-05 04:45:09 +0100

    Code Cleanup

--- Initialize/Initialize.asm
+++ Initialize/Initialize.asm
@@ -1,15 +1,14 @@
 ; N64 'Bare Metal' Initialize Demo by krom (Peter Lemon):
-
   include LIB\N64.INC ; Include N64 Definitions
-  dcb 2097152,$00 ; Set ROM Size
+  dcb 1048576,$00 ; Set ROM Size
   org $80000000 ; Entry Point Of Code
   include LIB\N64_HEADER.ASM  ; Include 64 Byte Header & Vector Table
   incbin LIB\N64_BOOTCODE.BIN ; Include 4032 Byte Boot Code
 
 Start:
-  lui t0,$BFC0 ; Initialize N64 (Stop N64 From Crashing 5 Seconds After Boot)
-  li t1,8
-  sw t1,$7FC(t0)
+  lui a0,PIF_BASE ; A0 = PIF Base Register ($BFC00000)
+  li t0,8
+  sw t0,(PIF_RAM+$3C)(a0)
 
 Loop:
   j Loop
