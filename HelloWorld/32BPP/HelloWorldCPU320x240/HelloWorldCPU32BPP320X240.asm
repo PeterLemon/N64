f7943fa9c8f384603462ef71432323894f57a6b2
PeterLemon <krom@gbadev.org>
2014-10-05 04:42:49 +0100

    Code Cleanup

--- HelloWorld/32BPP/HelloWorldCPU320x240/HelloWorldCPU32BPP320X240.asm
+++ HelloWorld/32BPP/HelloWorldCPU320x240/HelloWorldCPU32BPP320X240.asm
@@ -1,53 +1,53 @@
 ; N64 'Bare Metal' 32BPP 320x240 Hello World Demo by krom (Peter Lemon):
-
-PrintString: macro vram,xpos,ypos,fontfile,string,length ; Print Text String To VRAM Using Font At X,Y Position
-  lui t0,vram ; T0 = Frame Buffer Pointer
-  addi t0,((xpos*4)+((320*ypos)*4)) ; Place text at XY Position
-  la t1,fontfile ; T1 = Characters
-  la t2,string ; T2 = Text Offset
-  li t3,length ; T3 = Number of Text Characters to Print
-  DrawChars\@:
-    li t4,7 ; T4 = Character X Pixel Counter
-    li t5,7 ; T5 = Character Y Pixel Counter
-
-    lb t6,0(t2) ; T6 = Next Text Character
-    addi t2,1
-
-    sll t6,8 ; Add Shift to Correct Position in Font (* 256)
-    add t6,t1
-
-    DrawCharX\@:
-      lw t7,0(t6) ; Load Font Text Character Pixel
-      addi t6,4
-      sw t7,0(t0) ; Store Font Text Character Pixel into Frame Buffer
-      addi t0,4
-
-      bnez t4,DrawCharX\@ ; IF Character X Pixel Counter != 0 GOTO DrawCharX
-      subi t4,1 ; Decrement Character X Pixel Counter
-
-      addi t0,$4E0 ; Jump down 1 Scanline, Jump back 1 Char ((SCREEN_X * 4) - (CHAR_X * 4))
-      li t4,7 ; Reset Character X Pixel Counter
-      bnez t5,DrawCharX\@ ; IF Character Y Pixel Counter != 0 GOTO DrawCharX
-      subi t5,1 ; Decrement Character Y Pixel Counter
-
-    subi t0,$27E0 ; ((SCREEN_X * 4) * CHAR_Y) - CHAR_X * 4
-    bnez t3,DrawChars\@ ; Continue to Print Characters
-    subi t3,1 ; Subtract Number of Text Characters to Print
-    endm
-
   include LIB\N64.INC ; Include N64 Definitions
-  dcb 2097152,$00 ; Set ROM Size
+  dcb 1048576,$00 ; Set ROM Size
   org $80000000 ; Entry Point Of Code
   include LIB\N64_HEADER.ASM  ; Include 64 Byte Header & Vector Table
   incbin LIB\N64_BOOTCODE.BIN ; Include 4032 Byte Boot Code
 
-Start:
-  include LIB\N64_INIT.ASM ; Include Initialisation Routine
-  include LIB\N64_GFX.INC  ; Include Graphics Macros
+PrintString: macro vram, xpos, ypos, fontfile, string, length ; Print Text String To VRAM Using Font At X,Y Position
+  lui a0,vram ; A0 = Frame Buffer Pointer
+  addi a0,((xpos*4)+((320*ypos)*4)) ; Place text at XY Position
+  la a1,fontfile ; A1 = Characters
+  la a2,string ; A2 = Text Offset
+  li t0,length ; T0 = Number of Text Characters to Print
+  DrawChars\@:
+    li t1,7 ; T1 = Character X Pixel Counter
+    li t2,7 ; T2 = Character Y Pixel Counter
 
-  ScreenNTSC 320,240,BPP32,$A0100000 ; Screen NTSC: 320x240, 32BPP, DRAM Origin = $A0100000
-  PrintString $A010,128,32,FontBlack,Text,11 ; Print Text String To VRAM Using Font At X,Y Position
-  PrintString $A010,192,96,FontRed,Text,11 ; Print Text String To VRAM Using Font At X,Y Position
+    lb t3,0(a2) ; T3 = Next Text Character
+    addi a2,1
+
+    sll t3,8 ; Add Shift to Correct Position in Font (* 256)
+    add t3,a1
+
+    DrawCharX\@:
+      lw t4,0(t3) ; Load Font Text Character Pixel
+      addi t3,4
+      sw t4,0(a0) ; Store Font Text Character Pixel into Frame Buffer
+      addi a0,4
+
+      bnez t1,DrawCharX\@ ; IF Character X Pixel Counter != 0 GOTO DrawCharX
+      subi t1,1 ; Decrement Character X Pixel Counter
+
+      addi a0,$4E0 ; Jump down 1 Scanline, Jump back 1 Char ((SCREEN_X * 4) - (CHAR_X * 4))
+      li t1,7 ; Reset Character X Pixel Counter
+      bnez t2,DrawCharX\@ ; IF Character Y Pixel Counter != 0 GOTO DrawCharX
+      subi t2,1 ; Decrement Character Y Pixel Counter
+
+    subi a0,$27E0 ; ((SCREEN_X * 4) * CHAR_Y) - CHAR_X * 4
+    bnez t0,DrawChars\@ ; Continue to Print Characters
+    subi t0,1 ; Subtract Number of Text Characters to Print
+    endm
+
+Start:
+  include LIB\N64_GFX.INC ; Include Graphics Macros
+  N64_INIT ; Run N64 Initialisation Routine
+
+  ScreenNTSC 320, 240, BPP32, $A0100000 ; Screen NTSC: 320x240, 32BPP, DRAM Origin = $A0100000
+
+  PrintString $A010, 128, 32, FontBlack, Text, 11 ; Print Text String To VRAM Using Font At X,Y Position
+  PrintString $A010, 192, 96, FontRed, Text, 11 ; Print Text String To VRAM Using Font At X,Y Position
 
 Loop:
   j Loop
@@ -56,7 +56,7 @@ Loop:
 Text:
   db "Hello World!"
 
-  align 4 ; Align 32-bit
+  align 4 ; Align 32-Bit
 FontBlack:
   incbin FontBlack8x8.bin
 FontRed:
