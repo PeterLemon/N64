c2590f4c9edf7e7b5babe39383d1ad598d86b53b
PeterLemon <krom@gbadev.org>
2014-10-05 05:21:08 +0100

    Code Cleanup

--- Sound/DoubleShot/STEREO/8BIT/DoubleShotSTEREO8BIT.asm
+++ Sound/DoubleShot/STEREO/8BIT/DoubleShotSTEREO8BIT.asm
@@ -1,42 +1,41 @@
 ; N64 'Bare Metal' Sound Double Shot Stereo 8BIT Demo by krom (Peter Lemon):
-
   include LIB\N64.INC ; Include N64 Definitions
-  dcb 2097152,$00 ; Set ROM Size
+  dcb 1048576,$00 ; Set ROM Size
   org $80000000 ; Entry Point Of Code
   include LIB\N64_HEADER.ASM  ; Include 64 Byte Header & Vector Table
   incbin LIB\N64_BOOTCODE.BIN ; Include 4032 Byte Boot Code
 
 Start:
-  include LIB\N64_INIT.ASM ; Include Initialisation Routine
+  N64_INIT ; Run N64 Initialisation Routine
 
-  lui t0,AI_BASE ; AI Base Reg: Initialize Sound
-  li t1,1 ; Load AI Control DMA Enable Bit (1)
-  sw t1,AI_CONTROL(t0) ; Store AI Control DMA Enable Bit To AI Control Reg $A4500008
+  lui a0,AI_BASE ; A0 = AI Base Register ($A4500000)
+  li t0,1 ; T0 = AI Control DMA Enable Bit (1)
+  sw t0,AI_CONTROL(a0) ; Store AI Control DMA Enable Bit To AI Control Register ($A4500008)
 
-  la t1,Sample ; Sample DRAM Offset
-  sw t1,AI_DRAM_ADDR(t0) ; Store Sample DRAM Offset To AI DRAM Address Reg $A4500000
-  li t1,7 ; Load Sample Bit Rate (Bitrate-1)
-  sw t1,AI_BITRATE(t0) ; Store Sample Bit Rate To AI Bit Rate Reg $A4500014
+  la t0,Sample ; T0 = Sample DRAM Offset
+  sw t0,AI_DRAM_ADDR(a0) ; Store Sample DRAM Offset To AI DRAM Address Register ($A4500000)
+  li t0,7 ; T0 = Sample Bit Rate (Bitrate-1)
+  sw t0,AI_BITRATE(a0) ; Store Sample Bit Rate To AI Bit Rate Register ($A4500014)
 
-  li t1,(VI_NTSC_CLOCK/(44100/2))-1 ; Load Sample Frequency: (VI_NTSC_CLOCK(48681812) / FREQ(44100 / 2)) - 1
-  sw t1,AI_DACRATE(t0) ; Store Sample Frequency To AI DAC Rate Reg $A4500010
-  li t1,246288 ; Length Of Sample Buffer
-  sw t1,AI_LEN(t0) ; Store Length Of Sample Buffer To AI Length Reg $A4500004
+  li t0,(VI_NTSC_CLOCK/(44100/2))-1 ; T0 = Sample Frequency: (VI_NTSC_CLOCK(48681812) / FREQ(44100 / 2)) - 1
+  sw t0,AI_DACRATE(a0) ; Store Sample Frequency To AI DAC Rate Register ($A4500010)
+  li t0,246288 ; T0 = Length Of Sample Buffer
+  sw t0,AI_LEN(a0) ; Store Length Of Sample Buffer To AI Length Register ($A4500004)
 
 AIBusy:
-  lb t1,AI_STATUS(t0) ; Load AI Status Reg Byte $A450000C
-  andi t1,t1,$40 ; And AI Status With AI Status DMA Busy Bit ($40XXXXXX)
-  bnez t1,AIBusy ; If True AI DMA Is Busy
+  lb t0,AI_STATUS(a0) ; T0 = AI Status Register Byte ($A450000C)
+  andi t0,$40 ; AND AI Status With AI Status DMA Busy Bit ($40XXXXXX)
+  bnez t0,AIBusy ; IF TRUE AI DMA Is Busy
   nop ; Delay Slot
 
-  li t1,(VI_NTSC_CLOCK/(22050/2))-1 ; Load Sample Frequency: (VI_NTSC_CLOCK(48681812) / FREQ(22050 / 2)) - 1
-  sw t1,AI_DACRATE(t0) ; Store Sample Frequency To AI DAC Rate Reg $A4500010
-  li t1,246288 ; Length Of Sample Buffer
-  sw t1,AI_LEN(t0) ; Store Length Of Sample Buffer To AI Length Reg $A4500004
+  li t0,(VI_NTSC_CLOCK/(22050/2))-1 ; T0 = Sample Frequency: (VI_NTSC_CLOCK(48681812) / FREQ(22050 / 2)) - 1
+  sw t0,AI_DACRATE(a0) ; Store Sample Frequency To AI DAC Rate Register ($A4500010)
+  li t0,246288 ; T0 = Length Of Sample Buffer
+  sw t0,AI_LEN(a0) ; Store Length Of Sample Buffer To AI Length Register ($A4500004)
 
 Loop:
   j Loop
   nop ; Delay Slot
 
-Sample:
+Sample: ; 8-Bit 44100Hz Signed Stereo Sound Sample
   incbin SampleL.bin
