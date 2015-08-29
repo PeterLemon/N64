f74f0c89e0c0a0ec2df5e94036d75ba06fb0e8c7
PeterLemon <krom@gbadev.org>
2015-08-29 19:40:32 +0100

    Convert Source To BASS

--- Initialize/Initialize.asm
+++ Initialize/Initialize.asm
@@ -1,15 +1,20 @@
-; N64 'Bare Metal' Initialize Demo by krom (Peter Lemon):
-  include LIB\N64.INC ; Include N64 Definitions
-  dcb 1052672,$00 ; Set ROM Size
-  org $80000000 ; Entry Point Of Code
-  include LIB\N64_HEADER.ASM  ; Include 64 Byte Header & Vector Table
-  incbin LIB\N64_BOOTCODE.BIN ; Include 4032 Byte Boot Code
+// N64 'Bare Metal' Initialize Demo by krom (Peter Lemon):
+arch n64.cpu
+endian msb
+output "Initialize.N64", create
+fill 1052672 // Set ROM Size
+
+origin $00000000
+base $80000000 // Entry Point Of Code
+include "LIB\N64.INC" // Include N64 Definitions
+include "LIB\N64_HEADER.ASM" // Include 64 Byte Header & Vector Table
+insert "LIB\N64_BOOTCODE.BIN" // Include 4032 Byte Boot Code
 
 Start:
-  lui a0,PIF_BASE ; A0 = PIF Base Register ($BFC00000)
-  li t0,8
-  sw t0,(PIF_RAM+$3C)(a0)
+  lui a0,PIF_BASE // A0 = PIF Base Register ($BFC00000)
+  lli t0,8
+  sw t0,PIF_RAM+$3C(a0)
 
 Loop:
   j Loop
-  nop ; Delay Slot
+  nop // Delay Slot
--- Initialize/make.bat
+++ Initialize/make.bat
@@ -1,2 +1,2 @@
-asmn6432 /p Initialize.ASM,Initialize.N64
+bass Initialize.asm
 chksum64 Initialize.N64
