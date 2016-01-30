1ecc9205716dd722f2ef5caaa90b3b63e00c9351
PeterLemon <krom@gbadev.org>
2016-01-30 16:04:48 +0000

    RSP DMA Stride Example

--- RSP/DMAStride/RSPDMAStride.asm
+++ RSP/DMAStride/RSPDMAStride.asm
@@ -45,7 +45,7 @@ base $0000 // Set Base Of RSP Code Object To Zero
 RSPStart:
   li t0,((32*4)-1) | (31<<12) | (((320-32)*4)<<20) // T0 = Length Of DMA Transfer In Bytes - 1, DMA Line Count - 1, Line Skip/Stride
   lli a0,SP_DMEM // A0 = SP Memory Address Offset DMEM $010 ($A4000000..$A4001FFF 8KB)
-  li a1,$100000 | ((320*4)*104)+(144*4) // A1 = Aligned DRAM Physical RAM Offset ($00000000..$007FFFFF 8MB)
+  la a1,$100000 | ((320*4)*104)+(144*4) // A1 = Aligned DRAM Physical RAM Offset ($00000000..$007FFFFF 8MB)
   
   mtc0 a0,c0 // Store Memory Offset To SP Memory Address Register ($A4040000)
   mtc0 a1,c1 // Store RAM Offset To SP DRAM Address Register ($A4040004)
