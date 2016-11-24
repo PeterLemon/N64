c3d98b14b5e76c164f48b3ac3efbf68d69cb1667
PeterLemon <krom@gbadev.org>
2016-11-24 09:03:38 +0000

    RSP Fast Quantization Multi Block Demo

--- RSP/DCT/FastQuantizationMultiBlock16BIT/RSPFastQuantizationMultiBlock16BIT.asm
+++ RSP/DCT/FastQuantizationMultiBlock16BIT/RSPFastQuantizationMultiBlock16BIT.asm
@@ -469,8 +469,6 @@ LoopBlocks:
     nop // Delay Slot
 
   Finished:
-    nop // Delay Slot
-
   break // Set SP Status Halt, Broke & Check For Interrupt, Set SP Program Counter To $0000
 align(8) // Align 64-Bit
 base RSPCode+pc() // Set End Of RSP Code Object
