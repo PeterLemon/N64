009879743040ff5739f0b2c04bc0934ae80945bd
PeterLemon <krom@gbadev.org>
2017-10-22 05:17:02 +0100

    New RSP Lib

--- EMU/SNES/CPU/PPU/PPUXBPPRSP.asm
+++ EMU/SNES/CPU/PPU/PPUXBPPRSP.asm
@@ -81,19 +81,8 @@ base $0000 // Set Base Of RSP Code Object To Zero
 
 RSPTILEXBPPStart:
 // Load Static Shift Data
-  ori a0,r0,0 // A0 = Shift Start Offset
-  la a1,RSPSHIFTData // A1 = Aligned DRAM Physical RAM Offset ($00000000..$007FFFFF 8MB)
-  li t0,(RSPSHIFTDataEnd-RSPSHIFTData)-1 // T0 = Length Of DMA Transfer In Bytes - 1
-
-  mtc0 a0,c0 // Store Memory Offset To SP Memory Address Register ($A4040000)
-  mtc0 a1,c1 // Store RAM Offset To SP DRAM Address Register ($A4040004)
-  mtc0 t0,c2 // Store DMA Length To SP Read Length Register ($A4040008)
-
-  SHIFTDMAREADBusy:
-    mfc0 t0,c4 // T0 = RSP Status Register ($A4040010)
-    andi t0,RSP_BSY|RSP_FUL // AND RSP Status Status With $C (Bit 2 = DMA Is Busy, Bit 3 = DMA Is Full)
-    bnez t0,SHIFTDMAREADBusy // IF TRUE DMA Is Busy
-    nop // Delay Slot
+  RSPDMASPRD(RSPSHIFTData, RSPSHIFTDataEnd, SP_DMEM) // RSP DMA Data Read DRAM->RSP MEM: Start Address, End Address, Destination RSP MEM Address
+  RSPDMASPWait() // Wait For RSP DMA To Finish
 
   lqv v0[e0],ShiftLeftRightA(r0) // V0 = Left Shift Using Multiply: << 0..7,  Right Shift Using Multiply: >> 16..9 (128-Bit Quad)
   lqv v1[e0],ShiftLeftRightB(r0) // V1 = Left Shift Using Multiply: << 8..15, Right Shift Using Multiply: >> 8..1  (128-Bit Quad)
@@ -114,11 +103,7 @@ RSPTILEXBPPStart:
   mtc0 a1,c1 // Store RAM Offset To SP DRAM Address Register ($A4040004)
   mtc0 t0,c2 // Store DMA Length To SP Read Length Register ($A4040008)
 
-  PAL8BPPDMAREADBusy:
-    mfc0 t0,c4 // T0 = RSP Status Register ($A4040010)
-    andi t0,RSP_BSY|RSP_FUL // AND RSP Status Status With $C (Bit 2 = DMA Is Busy, Bit 3 = DMA Is Full)
-    bnez t0,PAL8BPPDMAREADBusy // IF TRUE DMA Is Busy
-    nop // Delay Slot
+  RSPDMASPWait() // Wait For RSP DMA To Finish
 
   ori t0,r0,30 // T0 = Color Counter
 
@@ -221,19 +206,8 @@ Loop2BPPColors:
 
 
   // DMA 8BPP & 4BPP TLUT
-  ori a0,r0,0 // A0 = SP Memory Address Offset DMEM ($A4000000..$A4001FFF 8KB)
-  la a1,N64TLUT8BPP // A1 = Aligned DRAM Physical RAM Offset ($00000000..$007FFFFF 8MB)
-  ori t0,r0,1023 // T0 = Length Of DMA Transfer In Bytes - 1
-
-  mtc0 a0,c0 // Store Memory Offset To SP Memory Address Register ($A4040000)
-  mtc0 a1,c1 // Store RAM Offset To SP DRAM Address Register ($A4040004)
-  mtc0 t0,c3 // Store DMA Length To SP Write Length Register ($A404000C)
-
-  PALDMAWRITEBusy:
-    mfc0 t0,c4 // T0 = RSP Status Register ($A4040010)
-    andi t0,RSP_BSY|RSP_FUL // AND RSP Status Status With $C (Bit 2 = DMA Is Busy, Bit 3 = DMA Is Full)
-    bnez t0,PALDMAWRITEBusy // IF TRUE DMA Is Busy
-    nop // Delay Slot
+  RSPDMASPWR(N64TLUT8BPP, N64TLUT8BPP+1024, SP_DMEM) // RSP DMA Data Write RSP MEM->DRAM: Start Address, End Address, Source RSP MEM Address
+  RSPDMASPWait() // Wait For RSP DMA To Finish
 
   // DMA With Stride 2BPP TLUT
   ori a0,r0,1024 // A0 = SP Memory Address Offset DMEM ($A4000000..$A4001FFF 8KB)
@@ -244,11 +218,7 @@ Loop2BPPColors:
   mtc0 a1,c1 // Store RAM Offset To SP DRAM Address Register ($A4040004)
   mtc0 t0,c3 // Store DMA Length To SP Write Length Register ($A404000C)
 
-  PAL2BPPDMAWRITEBusy:
-    mfc0 t0,c4 // T0 = RSP Status Register ($A4040010)
-    andi t0,RSP_BSY|RSP_FUL // AND RSP Status Status With $C (Bit 2 = DMA Is Busy, Bit 3 = DMA Is Full)
-    bnez t0,PAL2BPPDMAWRITEBusy // IF TRUE DMA Is Busy
-    nop // Delay Slot
+  RSPDMASPWait() // Wait For RSP DMA To Finish
 
 
 //-------------------
@@ -269,11 +239,7 @@ LoopTile2BPPBlocks:
   mtc0 a2,c1 // Store RAM Offset To SP DRAM Address Register ($A4040004)
   mtc0 t0,c2 // Store DMA Length To SP Read Length Register ($A4040008)
 
-  TILEDMAREAD2BPPBusy:
-    mfc0 t0,c4 // T0 = RSP Status Register ($A4040010)
-    andi t0,RSP_BSY|RSP_FUL // AND RSP Status Status With $C (Bit 2 = DMA Is Busy, Bit 3 = DMA Is Full)
-    bnez t0,TILEDMAREAD2BPPBusy // IF TRUE DMA Is Busy
-    nop // Delay Slot
+  RSPDMASPWait() // Wait For RSP DMA To Finish
 
 LoopTiles2BPP:
   lqv v3[e0],0(a0) // V3 = Tile BitPlane 0,1 Row 0..7
@@ -429,11 +395,7 @@ LoopTiles2BPP:
   mtc0 a1,c1 // Store RAM Offset To SP DRAM Address Register ($A4040004)
   mtc0 t0,c3 // Store DMA Length To SP Write Length Register ($A404000C)
 
-  TILEDMAWRITE2BPPBusy:
-    mfc0 t0,c4 // T0 = RSP Status Register ($A4040010)
-    andi t0,RSP_BSY|RSP_FUL // AND RSP Status Status With $C (Bit 2 = DMA Is Busy, Bit 3 = DMA Is Full)
-    bnez t0,TILEDMAWRITE2BPPBusy // IF TRUE DMA Is Busy
-    nop // Delay Slot
+  RSPDMASPWait() // Wait For RSP DMA To Finish
 
   addiu a1,32 // A1 = Next N64  Tile Offset
   addiu a2,16 // A2 = Next SNES Tile Offset
@@ -468,11 +430,7 @@ LoopTile4BPPBlocks:
   mtc0 a2,c1 // Store RAM Offset To SP DRAM Address Register ($A4040004)
   mtc0 t0,c2 // Store DMA Length To SP Read Length Register ($A4040008)
 
-  TILEDMAREAD4BPPBusy:
-    mfc0 t0,c4 // T0 = RSP Status Register ($A4040010)
-    andi t0,RSP_BSY|RSP_FUL // AND RSP Status Status With $C (Bit 2 = DMA Is Busy, Bit 3 = DMA Is Full)
-    bnez t0,TILEDMAREAD4BPPBusy // IF TRUE DMA Is Busy
-    nop // Delay Slot
+  RSPDMASPWait() // Wait For RSP DMA To Finish
 
 LoopTiles4BPP:
   lqv v3[e0],$00(a0) // V3 = Tile BitPlane 0,1 Row 0..7
@@ -683,11 +641,7 @@ LoopTiles4BPP:
   mtc0 a1,c1 // Store RAM Offset To SP DRAM Address Register ($A4040004)
   mtc0 t0,c3 // Store DMA Length To SP Write Length Register ($A404000C)
 
-  TILEDMAWRITE4BPPBusy:
-    mfc0 t0,c4 // T0 = RSP Status Register ($A4040010)
-    andi t0,RSP_BSY|RSP_FUL // AND RSP Status Status With $C (Bit 2 = DMA Is Busy, Bit 3 = DMA Is Full)
-    bnez t0,TILEDMAWRITE4BPPBusy // IF TRUE DMA Is Busy
-    nop // Delay Slot
+  RSPDMASPWait() // Wait For RSP DMA To Finish
 
   addiu a1,4096 // A1 = Next N64  Tile Offset
   addiu a2,4096 // A2 = Next SNES Tile Offset
@@ -713,11 +667,7 @@ LoopTile8BPPBlocks:
   mtc0 a2,c1 // Store RAM Offset To SP DRAM Address Register ($A4040004)
   mtc0 t0,c2 // Store DMA Length To SP Read Length Register ($A4040008)
 
-  TILEDMAREAD8BPPBusy:
-    mfc0 t0,c4 // T0 = RSP Status Register ($A4040010)
-    andi t0,RSP_BSY|RSP_FUL // AND RSP Status Status With $C (Bit 2 = DMA Is Busy, Bit 3 = DMA Is Full)
-    bnez t0,TILEDMAREAD8BPPBusy // IF TRUE DMA Is Busy
-    nop // Delay Slot
+  RSPDMASPWait() // Wait For RSP DMA To Finish
 
 LoopTiles8BPP:
   lqv v3[e0],$00(a0) // V3 = Tile BitPlane 0,1 Row 0..7
@@ -998,11 +948,7 @@ LoopTiles8BPP:
   mtc0 a1,c1 // Store RAM Offset To SP DRAM Address Register ($A4040004)
   mtc0 t0,c3 // Store DMA Length To SP Write Length Register ($A404000C)
 
-  TILEDMAWRITE8BPPBusy:
-    mfc0 t0,c4 // T0 = RSP Status Register ($A4040010)
-    andi t0,RSP_BSY|RSP_FUL // AND RSP Status Status With $C (Bit 2 = DMA Is Busy, Bit 3 = DMA Is Full)
-    bnez t0,TILEDMAWRITE8BPPBusy // IF TRUE DMA Is Busy
-    nop // Delay Slot
+  RSPDMASPWait() // Wait For RSP DMA To Finish
 
   addiu a1,4096 // A1 = Next N64  Tile Offset
   addiu a2,4096 // A2 = Next SNES Tile Offset
