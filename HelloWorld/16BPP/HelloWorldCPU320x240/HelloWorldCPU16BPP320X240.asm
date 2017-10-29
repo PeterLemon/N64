659bf867c98896bcc40b177a1fccc2b78906a513
PeterLemon <krom@gbadev.org>
2017-10-29 07:04:39 +0000

    Hello World 16BPP CPU Demo Added

--- HelloWorld/16BPP/HelloWorldCPU320x240/HelloWorldCPU16BPP320X240.asm
+++ HelloWorld/16BPP/HelloWorldCPU320x240/HelloWorldCPU16BPP320X240.asm
@@ -21,36 +21,36 @@ insert "LIB/N64_BOOTCODE.BIN" // Include 4032 Byte Boot Code
 
 macro PrintString(vram, xpos, ypos, fontfile, string, length) { // Print Text String To VRAM Using Font At X,Y Position
   li a0,{vram}+({xpos}*BYTES_PER_PIXEL)+(SCREEN_X*BYTES_PER_PIXEL*{ypos}) // A0 = Frame Buffer Pointer (Place text at XY Position)
-  la a1,{fontfile} // A1 = Characters
-  la a2,{string} // A2 = Text Offset
-  lli t0,{length} // T0 = Number of Text Characters to Print
+  la a1,{fontfile} // A1 = Font Address
+  la a2,{string} // A2 = Text Address
+  ori t0,r0,{length} // T0 = Number of Text Characters to Print
   {#}DrawChars:
-    lli t1,CHAR_X-1 // T1 = Character X Pixel Counter
-    lli t2,CHAR_Y-1 // T2 = Character Y Pixel Counter
+    ori t1,r0,CHAR_X-1 // T1 = Character X Pixel Counter
+    ori t2,r0,CHAR_Y-1 // T2 = Character Y Pixel Counter
 
-    lb t3,0(a2) // T3 = Next Text Character
-    addi a2,1
+    lbu t3,0(a2) // T3 = Next Text Character
+    addiu a2,1 // Text Address++
 
-    sll t3,7 // Add Shift to Correct Position in Font (*128: CHAR_X*CHAR_Y*BYTES_PER_PIXEL)
-    add t3,a1
+    sll t3,7 // T3 *= 128 (Shift to Correct Position in Font)
+    addu t3,a1 // T3 += Font Address
 
     {#}DrawCharX:
       lw t4,0(t3) // Load Font Text Character Pixel
-      addi t3,BYTES_PER_PIXEL
+      addiu t3,BYTES_PER_PIXEL
       sw t4,0(a0) // Store Font Text Character Pixel into Frame Buffer
-      addi a0,BYTES_PER_PIXEL
+      addiu a0,BYTES_PER_PIXEL
 
       bnez t1,{#}DrawCharX // IF (Character X Pixel Counter != 0) DrawCharX
-      subi t1,1 // Decrement Character X Pixel Counter
+      subiu t1,1 // Decrement Character X Pixel Counter (Delay Slot)
 
-      addi a0,(SCREEN_X*BYTES_PER_PIXEL)-CHAR_X*BYTES_PER_PIXEL // Jump Down 1 Scanline, Jump Back 1 Char
-      lli t1,CHAR_X-1 // Reset Character X Pixel Counter
+      addiu a0,(SCREEN_X*BYTES_PER_PIXEL)-CHAR_X*BYTES_PER_PIXEL // Jump Down 1 Scanline, Jump Back 1 Char
+      ori t1,r0,CHAR_X-1 // Reset Character X Pixel Counter
       bnez t2,{#}DrawCharX // IF (Character Y Pixel Counter != 0) DrawCharX
-      subi t2,1 // Decrement Character Y Pixel Counter
+      subiu t2,1 // Decrement Character Y Pixel Counter (Delay Slot)
 
-    subi a0,((SCREEN_X*BYTES_PER_PIXEL)*CHAR_Y)-CHAR_X*BYTES_PER_PIXEL // Jump To Start Of Next Char
+    subiu a0,((SCREEN_X*BYTES_PER_PIXEL)*CHAR_Y)-CHAR_X*BYTES_PER_PIXEL // Jump To Start Of Next Char
     bnez t0,{#}DrawChars // Continue to Print Characters
-    subi t0,1 // Subtract Number of Text Characters to Print
+    subiu t0,1 // Subtract Number of Text Characters to Print (Delay Slot)
 }
 
 Start:
