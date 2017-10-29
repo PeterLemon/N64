b08815b853a0ec479121ff77dcb3c6d80c442ac5
PeterLemon <krom@gbadev.org>
2017-10-29 01:42:38 +0000

    Hello World 16BPP CPU Demo Added

---
+++ HelloWorld/16BPP/HelloWorldCPU320x240/HelloWorldCPU16BPP320X240.asm
@@ -0,0 +1,74 @@
+// N64 'Bare Metal' 16BPP 320x240 Hello World Demo by krom (Peter Lemon):
+arch n64.cpu
+endian msb
+output "HelloWorldCPU16BPP320X240.N64", create
+fill 1052672 // Set ROM Size
+
+// Setup Frame Buffer
+constant SCREEN_X(320)
+constant SCREEN_Y(240)
+constant BYTES_PER_PIXEL(2)
+
+// Setup Characters
+constant CHAR_X(8)
+constant CHAR_Y(8)
+
+origin $00000000
+base $80000000 // Entry Point Of Code
+include "LIB/N64.INC" // Include N64 Definitions
+include "LIB/N64_HEADER.ASM" // Include 64 Byte Header & Vector Table
+insert "LIB/N64_BOOTCODE.BIN" // Include 4032 Byte Boot Code
+
+macro PrintString(vram, xpos, ypos, fontfile, string, length) { // Print Text String To VRAM Using Font At X,Y Position
+  li a0,{vram}+({xpos}*BYTES_PER_PIXEL)+(SCREEN_X*BYTES_PER_PIXEL*{ypos}) // A0 = Frame Buffer Pointer (Place text at XY Position)
+  la a1,{fontfile} // A1 = Characters
+  la a2,{string} // A2 = Text Offset
+  lli t0,{length} // T0 = Number of Text Characters to Print
+  {#}DrawChars:
+    lli t1,CHAR_X-1 // T1 = Character X Pixel Counter
+    lli t2,CHAR_Y-1 // T2 = Character Y Pixel Counter
+
+    lb t3,0(a2) // T3 = Next Text Character
+    addi a2,1
+
+    sll t3,7 // Add Shift to Correct Position in Font (*256: CHAR_X*CHAR_Y*BYTES_PER_PIXEL)
+    add t3,a1
+
+    {#}DrawCharX:
+      lw t4,0(t3) // Load Font Text Character Pixel
+      addi t3,BYTES_PER_PIXEL
+      sw t4,0(a0) // Store Font Text Character Pixel into Frame Buffer
+      addi a0,BYTES_PER_PIXEL
+
+      bnez t1,{#}DrawCharX // IF (Character X Pixel Counter != 0) DrawCharX
+      subi t1,1 // Decrement Character X Pixel Counter
+
+      addi a0,(SCREEN_X*BYTES_PER_PIXEL)-CHAR_X*BYTES_PER_PIXEL // Jump Down 1 Scanline, Jump Back 1 Char
+      lli t1,CHAR_X-1 // Reset Character X Pixel Counter
+      bnez t2,{#}DrawCharX // IF (Character Y Pixel Counter != 0) DrawCharX
+      subi t2,1 // Decrement Character Y Pixel Counter
+
+    subi a0,((SCREEN_X*BYTES_PER_PIXEL)*CHAR_Y)-CHAR_X*BYTES_PER_PIXEL // Jump To Start Of Next Char
+    bnez t0,{#}DrawChars // Continue to Print Characters
+    subi t0,1 // Subtract Number of Text Characters to Print
+}
+
+Start:
+  include "LIB/N64_GFX.INC" // Include Graphics Macros
+  N64_INIT() // Run N64 Initialisation Routine
+
+  ScreenNTSC(320, 240, BPP16, $A0100000) // Screen NTSC: 320x240, 16BPP, DRAM Origin = $A0100000
+
+  PrintString($A0100000, 128, 32, FontBlack, Text, 11) // Print Text String To VRAM Using Font At X,Y Position
+  PrintString($A0100000, 192, 96, FontRed, Text, 11) // Print Text String To VRAM Using Font At X,Y Position
+
+Loop:
+  j Loop
+  nop // Delay Slot
+
+Text:
+  db "Hello World!"
+
+align(4) // Align 32-Bit
+insert FontBlack, "FontBlack8x8.bin"
+insert FontRed, "FontRed8x8.bin"
---
+++ HelloWorld/16BPP/HelloWorldCPU320x240/make.bat
@@ -0,0 +1,2 @@
+bass HelloWorldCPU16BPP320X240.asm
+chksum64 HelloWorldCPU16BPP320X240.N64
