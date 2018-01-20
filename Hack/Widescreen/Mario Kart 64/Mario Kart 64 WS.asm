da6e84e67170f71b850b9de0d1a3caaa3e921491
PeterLemon <krom@gbadev.org>
2018-01-20 21:41:14 +0000

    Add Mario Kart 64 Widescreen Hack by gamemasterplc

---
+++ Hack/Widescreen/Mario Kart 64/Mario Kart 64 WS.asm
@@ -0,0 +1,22 @@
+// N64 "Mario Kart 64" Widescreen ROM Hack by gamemasterplc:
+
+arch n64.cpu
+endian msb // N64 MIPS requires Big-Endian Encoding (Most Significant Bit)
+output "Mario Kart 64 WS.z64", create
+origin $00000000; insert "Mario Kart 64 (U) [!].z64" // Include USA Mario Kart 64 N64 ROM
+origin $00000020
+db "MARIOKART64 WS             " // $00000020 - PROGRAM TITLE (27 Byte ASCII String, Use Spaces For Unused Bytes)
+
+include "LIB/N64.INC" // Include N64 Definitions
+
+origin $000952B8
+li a3, 0x3FE38E39  //Rotating Nintendo Logo Aspect Ratio (1.77777f)
+
+origin $000953EC
+li a3, 0x3FE38E39 //Title Screen Flag Aspect Ratio (1.77777f)
+
+origin $0012322C
+float32 1.777777 //1 Player Mode Aspect Ratio
+float32 0.888888 //2 Player Vertical Splitscreen Aspect Ratio
+float32 3.555555 //2 Player Horizontal Splitscreen Aspect Ratio
+float32 1.777777 //3 Player and 4 Player Mode Aspect Ratio
---
+++ Hack/Widescreen/Mario Kart 64/make.bat
@@ -0,0 +1,2 @@
+bass "Mario Kart 64 WS.asm"
+ucon64 --nbak --chk "Mario Kart 64 WS.z64"
