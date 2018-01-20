420212ede645805eb50ae033dd4d7f75863b3789
PeterLemon <krom@gbadev.org>
2018-01-20 01:13:39 +0000

    Add Wave Race 64 Widescreen Hack

---
+++ Hack/Widescreen/Wave Race 64/Wave Race 64 WS.asm
@@ -0,0 +1,35 @@
+// N64 "Wave Race 64" Widescreen Hack by krom (Peter Lemon):
+// Special thanks to gamemasterplc
+
+arch n64.cpu
+endian msb // N64 MIPS requires Big-Endian Encoding (Most Significant Bit)
+output "Wave Race 64 WS.z64", create
+origin $00000000; insert "Wave Race 64 (U) (V1.1) [!].z64" // Include USA Wave Race 64 N64 ROM
+origin $00000020
+db "WAVE RACE 64 WS            " // $00000020 - PROGRAM TITLE (27 Byte ASCII String, Use Spaces For Unused Bytes)
+
+include "LIB/N64.INC" // Include N64 Definitions
+
+origin $000241F8
+             // ROM       RAM         Widescreen (Streched)
+lui a3,$3FE3 // $000241F8 $800699F8 - (4 Bytes) (LUI A3,$3F80: $3C073F80)
+
+origin $000AF820
+             // ROM       RAM         Widescreen (Streched)
+lui a3,$3FE3 // $000AF820 $801E11F0 - (4 Bytes) (LUI A3,$3FAA: $3C073FAA)
+
+origin $000BDA20
+             // ROM       RAM         Widescreen (Streched)
+lui a3,$3FE3 // $000BDA20 $801EF3F0 - (4 Bytes) (LUI A3,$3FAA: $3C073FAA)
+
+origin $000BEE14
+             // ROM       RAM         Widescreen (Streched)
+lui a3,$3FE3 // $000BEE14 $801F07E4 - (4 Bytes) (LUI A3,$3FAA: $3C073FAA)
+
+origin $001B94D0
+             // ROM       RAM         Widescreen (Streched)
+lui a3,$3FAA // $001B94D0 $802C5890 - (4 Bytes) (MFC1 A3,F28: $4407E000)
+
+origin $001B9504
+             // ROM       RAM         Widescreen (Streched)
+lui a3,$3FAA // $001B9504 $802C58C4 - (4 Bytes) (MFC1 A3,F28: $4407E000)
---
+++ Hack/Widescreen/Wave Race 64/make.bat
@@ -0,0 +1,2 @@
+bass "Wave Race 64 WS.asm"
+ucon64 --nbak --chk "Wave Race 64 WS.z64"
