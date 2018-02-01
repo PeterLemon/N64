29127be67be7fc3c579c1a72e5a09b14cecb5986
PeterLemon <krom@gbadev.org>
2018-02-01 23:19:48 +0000

    Add Clay Fighter - Sculptors Cut Widescreen Hack

---
+++ Hack/Widescreen/Clay Fighter - Sculptor's Cut/Clay Fighter - Sculptor's Cut WS.asm
@@ -0,0 +1,16 @@
+// N64 "Clay Fighter - Sculptor's Cut" Widescreen Hack by krom (Peter Lemon):
+// Special thanks to theboy181
+
+arch n64.cpu
+endian msb // N64 MIPS requires Big-Endian Encoding (Most Significant Bit)
+output "Clay Fighter - Sculptor's Cut WS.z64", create
+origin $00000000; insert "Clay Fighter - Sculptor's Cut (U) [!].z64" // Include USA Clay Fighter - Sculptor's Cut N64 ROM
+origin $00000020
+db "Clayfighter SC WS          " // $00000020 - PROGRAM TITLE (27 Byte ASCII String, Use Spaces For Unused Bytes)
+
+include "LIB/N64.INC" // Include N64 Definitions
+
+origin $00080B58
+             // ROM       RAM         Widescreen (Streched)
+lui a3,$3FE3 // $00080B58 $8007FF58 - (4 Bytes) (LUI A3,$3FAA: $3C073FAA)
+ori a3,$8E39 // $00080B5C $8007FF5C - (4 Bytes) (ORI A3,$AAAB: $34E7AAAB)
---
+++ Hack/Widescreen/Clay Fighter - Sculptor's Cut/make.bat
@@ -0,0 +1,2 @@
+bass "Clay Fighter - Sculptor's Cut WS.asm"
+ucon64 --nbak --chk "Clay Fighter - Sculptor's Cut WS.z64"
