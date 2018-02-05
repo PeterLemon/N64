786b7a115ca6a347bef71004973ada5ae6b8e0da
PeterLemon <krom@gbadev.org>
2018-02-05 16:31:58 +0000

    Add Nightmare Creatures Widescreen Hack

---
+++ Hack/Widescreen/Nightmare Creatures/Nightmare Creatures WS.asm
@@ -0,0 +1,15 @@
+// N64 "Nightmare Creatures" Widescreen Hack by krom (Peter Lemon):
+// Special thanks to theboy181
+
+arch n64.cpu
+endian msb // N64 MIPS requires Big-Endian Encoding (Most Significant Bit)
+output "Nightmare Creatures WS.z64", create
+origin $00000000; insert "Nightmare Creatures (U) [!].z64" // Include USA Nightmare Creatures N64 ROM
+origin $00000020
+db "NIGHTMARE CREATURES WS     " // $00000020 - PROGRAM TITLE (27 Byte ASCII String, Use Spaces For Unused Bytes)
+
+include "LIB/N64.INC" // Include N64 Definitions
+
+origin $00075914
+             // ROM       RAM         Widescreen (Streched)
+lui a3,$3FE3 // $00075914 $80074D14 - (4 Bytes) (LUI A3,$3FAA: $3C073FAA)
---
+++ Hack/Widescreen/Nightmare Creatures/make.bat
@@ -0,0 +1,2 @@
+bass "Nightmare Creatures WS.asm"
+ucon64 --nbak --chk "Nightmare Creatures WS.z64"
