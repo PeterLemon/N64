fd3336ad07375efda4b733b4429d295c385f1fcb
PeterLemon <krom@gbadev.org>
2018-02-25 15:36:23 +0000

    Add Diddy Kong Racing Widescreen Hack

--- Hack/Widescreen/Diddy Kong Racing/Diddy Kong Racing WS.asm
+++ Hack/Widescreen/Diddy Kong Racing/Diddy Kong Racing WS.asm
@@ -11,19 +11,16 @@ db "Diddy Kong Racing WS       " // $00000020 - PROGRAM TITLE (27 Byte ASCII Str
 include "LIB/N64.INC" // Include N64 Definitions
 
 origin $00066E44
-             // ROM       RAM         Widescreen (Streched)
-lui a3,$3FE3 // $00066E44 $80066244 - (4 Bytes) (LUI A3,$3FAA: $3C073FAA)
-ori a3,$8E39 // $00066E48 $80066248 - (4 Bytes) (ORI A3,$AAAB: $34E7AAAB)
+                // ROM       RAM         Widescreen (Streched)
+li a3,$3FE38E39 // $00066E44 $80066244 - (8 Bytes) (LUI A3,$3FAA: $3C073FAA, ORI A3,$AAAB: $34E7AAAB)
 
 origin $00066F94
-             // ROM       RAM         Widescreen (Streched)
-lui a3,$3FE3 // $00066F94 $80066394 - (4 Bytes) (LUI A3,$3FAA: $3C073FAA)
-ori a3,$8E39 // $00066F98 $80066398 - (4 Bytes) (ORI A3,$AAAB: $34E7AAAB)
+                // ROM       RAM         Widescreen (Streched)
+li a3,$3FE38E39 // $00066F94 $80066394 - (8 Bytes) (LUI A3,$3FAA: $3C073FAA, ORI A3,$AAAB: $34E7AAAB)
 
 origin $00066FFC
-             // ROM       RAM         Widescreen (Streched)
-lui a3,$3FE3 // $00066FFC $800663FC - (4 Bytes) (LUI A3,$3FAA: $3C073FAA)
-ori a3,$8E39 // $00067000 $80066400 - (4 Bytes) (ORI A3,$AAAB: $34E7AAAB)
+                // ROM       RAM         Widescreen (Streched)
+li a3,$3FE38E39 // $00066FFC $800663FC - (8 Bytes) (LUI A3,$3FAA: $3C073FAA, ORI A3,$AAAB: $34E7AAAB)
 
 origin $000E8C98
                    // ROM       RAM         Widescreen (Streched)
