74a6d68b2325d0c4858e28c4a2a9082861063f65
peterlemon <krom@gbadev.org>
2018-06-03 09:55:08 +0100

    RSP DCT ZigZag Quantization DCT Block Decode Demo

--- RSP/DCT/FastZigZagQuantizationBlockDecode/RSPFastZigZagQuantizationBlockDecode.asm
+++ RSP/DCT/FastZigZagQuantizationBlockDecode/RSPFastZigZagQuantizationBlockDecode.asm
@@ -22,6 +22,14 @@ Start:
   // Perform Inverse ZigZag Transformation On DCT Block Using RDP
   DPC(RDPBuffer, RDPBufferEnd) // Run DPC Command Buffer: Start, End
 
+  // Wait For RDP To Inverse ZigZag Block Before Running RSP
+  lui a0,DPC_BASE // A0 = DP Command (DPC) Base Register ($04100000)
+  li t0,(RDPBufferEnd & 0xFFFFFF) // Wait For RDP Commands
+  ZigZagLoop:
+    lwu t1,DPC_CURRENT(a0) // T1 = CMD DMA Current ($04100008)
+    blt t1,t0,ZigZagLoop // IF (T1 < T0) ZigZagLoop
+    nop // Delay Slot
+
   // Load RSP Code To IMEM
   DMASPRD(RSPCode, RSPCodeEnd, SP_IMEM) // DMA Data Read DRAM->RSP MEM: Start Address, End Address, Destination RSP MEM Address
   DMASPWait() // Wait For RSP DMA To Finish
