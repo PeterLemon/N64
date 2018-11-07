29666a9b6aaac65acab6ba2f3f0e2099088323b3
peterlemon <krom@gbadev.org>
2018-11-07 11:51:53 +0000

    Fix RSP PC Thanks To Rasky

--- RSP/XBUS/RSPPlotTriangle/RSPPlotTriangle.asm
+++ RSP/XBUS/RSPPlotTriangle/RSPPlotTriangle.asm
@@ -397,7 +397,7 @@ RSPStart:
 
   RSPDPC(RDPBuffer, RDPBufferEnd) // Run DPC Command Buffer: Start, End
 
-  break // Set SP Status Halt, Broke & Check For Interrupt, Set SP Program Counter To $0000
+  break // Set SP Status Halt, Broke & Check For Interrupt
 align(8) // Align 64-Bit
 base RSPCode+pc() // Set End Of RSP Code Object
 RSPCodeEnd:
