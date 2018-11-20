9e45f572ae94302617c946c780e16f6a6125574e
peterlemon <krom@gbadev.org>
2018-11-20 15:59:20 +0000

    YUV 8 RLE Video Demo small fix

--- Video/YUV8RLEVideo/YUV8RLEVideo.asm
+++ Video/YUV8RLEVideo/YUV8RLEVideo.asm
@@ -6,6 +6,8 @@ fill 1052672 // Set ROM Size
 
 constant YUV($80200000) // YUV Frame DRAM Offset
 
+constant FRAMES(1295) // Number Of Frames
+
 origin $00000000
 base $80000000 // Entry Point Of Code
 include "LIB/N64.INC" // Include N64 Definitions
@@ -34,9 +36,20 @@ Start:
   DMASPRD(RSPCode, RSPCodeEnd, SP_IMEM) // DMA Data Read DRAM->RSP MEM: Start Address, End Address, Destination RSP MEM Address
   DMASPWait() // Wait For RSP DMA To Finish
 
+  lui a0,AI_BASE // A0 = AI Base Register ($A4500000)
+  ori t0,r0,1 // T0 = AI Control DMA Enable Bit (1)
+  sw t0,AI_CONTROL(a0) // Store AI Control DMA Enable Bit To AI Control Register ($A4500008)
+  ori t0,r0,15 // T0 = Sample Bit Rate (Bitrate-1)
+  sw t0,AI_BITRATE(a0) // Store Sample Bit Rate To AI Bit Rate Register ($A4500014)
+  li t0,(VI_NTSC_CLOCK/44100)-1 // T0 = Sample Frequency: (VI_NTSC_CLOCK(48681812) / FREQ(44100)) - 1
+  sw t0,AI_DACRATE(a0) // Store Sample Frequency To AI DAC Rate Register ($A4500010)
+
 LoopVideo:
-  lli t9,1295-1 // T9 = Frame Count - 1
-  la a3,$10000000|(RLEVideo&$3FFFFFF) // A3 = Aligned Cart Physical ROM Offset ($10000000..$13FFFFFF 64MB)
+  la t6,Sample // T6 = Sample DRAM Offset
+  la t7,$10000000|(Sample&$FFFFFFF) // T7 = Sample Aligned Cart Physical ROM Offset ($10000000..$1FFFFFFF 256MB)
+
+  lli t9,FRAMES-1 // T9 = Frame Count - 1
+  la a3,$10000000|(RLEVideo&$FFFFFFF) // A3 = Aligned Cart Physical ROM Offset ($10000000..$1FFFFFFF 256MB)
   
   LoopFrames:
     lui a0,PI_BASE // A0 = PI Base Register ($A4600000)
@@ -47,6 +60,26 @@ LoopVideo:
     sw t0,PI_WR_LEN(a0) // Store DMA Length To PI Write Length Register ($A460000C)
 
     WaitScanline($1E0) // Wait For Scanline To Reach Vertical Blank
+//    WaitScanline($1E2) // Wait For Scanline To Reach Vertical Blank
+
+    // Buffer Sound
+    lui a0,AI_BASE // A0 = AI Base Register ($A4500000)
+    lb t0,AI_STATUS(a0) // T0 = AI Status Register Byte ($A450000C)
+    andi t0,$40 // AND AI Status With AI Status DMA Busy Bit ($40XXXXXX)
+    bnez t0,AIBusy // IF TRUE AI DMA Is Busy
+    nop // Delay Slot
+
+    lui a0,PI_BASE // A0 = PI Base Register ($A4600000)
+    sw t6,PI_DRAM_ADDR(a0) // Store RAM Offset To PI DRAM Address Register ($A4600000)
+    sw t7,PI_CART_ADDR(a0) // Store ROM Offset To PI Cart Address Register ($A4600004)
+    lli t0,$3FFF // T0 = Length Of DMA Transfer In Bytes - 1
+    sw t0,PI_WR_LEN(a0) // Store DMA Length To PI Write Length Register ($A460000C)
+
+    lui a0,AI_BASE // A0 = AI Base Register ($A4500000)
+    sw t6,AI_DRAM_ADDR(a0) // Store Sample DRAM Offset To AI DRAM Address Register ($A4500000)
+    sw t0,AI_LEN(a0) // Store Length Of Sample Buffer To AI Length Register ($A4500004)
+    add t7,t0 // Sample ROM Offset += $3FFF
+    AIBusy:
 
     la a0,RLEVideo+4 // A0 = Source Address (ROM Start Offset) ($B0000000..$B3FFFFFF)
     lui a1,YUV>>16 // A1 = Destination Address (DRAM Start Offset)
@@ -109,6 +142,9 @@ LoopVideo:
     bne a0,a1,LoopCache
     addiu a0,16 // Address += Data Line Size (Delay Slot)
 
+
+//  WaitScanline($1E0) // Wait For Scanline To Reach Vertical Blank
+//  WaitScanline($1E2) // Wait For Scanline To Reach Vertical Blank
 
   // Perform Inverse ZigZag Transformation On DCT Blocks Using RDP
   DPC(RDPZigZagBuffer, RDPZigZagBufferEnd) // Run DPC Command Buffer: Start, End
@@ -717,4 +753,5 @@ arch n64.rdp
   Sync_Full // Ensure Entire Scene Is Fully Drawn
 RDPYUVBufferEnd:
 
+insert Sample, "Sample.bin" // 16-Bit 44100Hz Signed Big-Endian Stereo Sound Sample
 insert RLEVideo, "Video.rle" // 1295 320x240 RLE Compressed YUV Frames 
