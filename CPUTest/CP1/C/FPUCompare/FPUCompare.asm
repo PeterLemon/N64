b83cb8fd1401485d8b525e86feb79f35d8ebc1e7
peterlemon <krom@gbadev.org>
2020-12-03 03:05:46 +0000

    FPUCompare test by Lemmy

---
+++ CPUTest/CP1/C/FPUCompare/FPUCompare.asm
@@ -0,0 +1,113 @@
+scope c.f.s {
+  define operation(c.f.s)
+
+  define header_title_27("FPU Compare c.f.s          ")
+  include "FPUCompare.inc"
+}
+
+scope c.un.s {
+  define operation(c.un.s)
+
+  define header_title_27("FPU Compare c.un.s         ")
+  include "FPUCompare.inc"
+}
+
+scope c.eq.s {
+  define operation(c.eq.s)
+
+  define header_title_27("FPU Compare c.eq.s         ")
+  include "FPUCompare.inc"
+}
+
+scope c.ueq.s {
+  define operation(c.ueq.s)
+
+  define header_title_27("FPU Compare c.ueq.s        ")
+  include "FPUCompare.inc"
+}
+
+scope c.olt.s {
+  define operation(c.olt.s)
+
+  define header_title_27("FPU Compare c.olt.s        ")
+  include "FPUCompare.inc"
+}
+
+scope c.ult.s {
+  define operation(c.ult.s)
+
+  define header_title_27("FPU Compare c.ult.s        ")
+  include "FPUCompare.inc"
+}
+
+scope c.ole.s {
+  define operation(c.ole.s)
+
+  define header_title_27("FPU Compare c.ole.s        ")
+  include "FPUCompare.inc"
+}
+
+scope c.ule.s {
+  define operation(c.ule.s)
+
+  define header_title_27("FPU Compare c.ule.s        ")
+  include "FPUCompare.inc"
+}
+
+
+scope c.sf.s {
+  define operation(c.sf.s)
+
+  define header_title_27("FPU Compare c.sf.s         ")
+  include "FPUCompare.inc"
+}
+
+scope c.ngle.s {
+  define operation(c.ngle.s)
+
+  define header_title_27("FPU Compare c.ngle.s       ")
+  include "FPUCompare.inc"
+}
+
+scope c.seq.s {
+  define operation(c.seq.s)
+
+  define header_title_27("FPU Compare c.seq.s        ")
+  include "FPUCompare.inc"
+}
+
+scope c.ngl.s {
+  define operation(c.ngl.s)
+
+  define header_title_27("FPU Compare c.ngl.s        ")
+  include "FPUCompare.inc"
+}
+
+scope c.lt.s {
+  define operation(c.lt.s)
+
+  define header_title_27("FPU Compare c.lt.s         ")
+  include "FPUCompare.inc"
+}
+
+scope c.nge.s {
+  define operation(c.nge.s)
+
+  define header_title_27("FPU Compare c.nge.s        ")
+  include "FPUCompare.inc"
+}
+
+scope c.le.s {
+  define operation(c.le.s)
+
+  define header_title_27("FPU Compare c.le.s         ")
+  include "FPUCompare.inc"
+}
+
+scope c.ngt.s {
+  define operation(c.ngt.s)
+
+  define header_title_27("FPU Compare c.ngt.s        ")
+  include "FPUCompare.inc"
+}
+
---
+++ CPUTest/CP1/C/FPUCompare/make.bat
@@ -0,0 +1,17 @@
+bass FPUCompare.asm
+chksum64 FPUCompare-c.eq.s.N64
+chksum64 FPUCompare-c.f.s.N64
+chksum64 FPUCompare-c.le.s.N64
+chksum64 FPUCompare-c.lt.s.N64
+chksum64 FPUCompare-c.nge.s.N64
+chksum64 FPUCompare-c.ngl.s.N64
+chksum64 FPUCompare-c.ngle.s.N64
+chksum64 FPUCompare-c.ngt.s.N64
+chksum64 FPUCompare-c.ole.s.N64
+chksum64 FPUCompare-c.olt.s.N64
+chksum64 FPUCompare-c.seq.s.N64
+chksum64 FPUCompare-c.sf.s.N64
+chksum64 FPUCompare-c.ueq.s.N64
+chksum64 FPUCompare-c.ule.s.N64
+chksum64 FPUCompare-c.ult.s.N64
+chksum64 FPUCompare-c.un.s.N64
